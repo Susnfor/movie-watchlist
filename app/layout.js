16915050b9ede0082f7580d017bbea9849f1a815
susnfor <97429786+Susnfor@users.noreply.github.com>
2025-06-11 20:09:12 +0100

    chore: add MUI and Emotion dependencies for UI components

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider>
+        {props.children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
